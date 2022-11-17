b03266bd31c50b4300253d62cece2c0a281b3cf0
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2022-11-17 02:00:37 +0200

    Test commit, some work done!

---
+++ HouseRentingSystem.Infrastructure/Data/Agent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HouseRentingSystem.Infrastructure.Data
+{
+    public class Agent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; } = null!;
+    }
+}
+
+//•	Id – a unique integer, Primary Key
+//•	PhoneNumber – a string with min length 7 and max length 15 (required)
+//•	UserId – a string(required)
+//•	User – an IdentityUser object
+
---
+++ HouseRentingSystem.Infrastructure/Data/House.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HouseRentingSystem.Infrastructure.Data
+{
+    public class House
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(50)]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [StringLength(200)]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = "money")]
+        [Precision(18,2)]
+        public decimal PricePerMonth { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+
+        [Required]
+        public int AgentId { get; set; }
+
+        [ForeignKey(nameof(AgentId))]
+        public Agent Agent { get; set; } = null!;
+
+        public string? RenterId { get; set; }
+
+        [ForeignKey(nameof(RenterId))]
+        public IdentityUser? Renter { get; set; }
+    }
+}
+
+//•	Id – a unique integer, Primary Key
+//•	Title – a string with min length 10 and max length 50 (required)
+//•	Address – a string with min length 30 and max length 150 (required)
+//•	Description – a string with min length 50 and max length 500 (required)
+//•	ImageUrl – a string(required)
+//•	PricePerMonth – a decimal with min value 0 and max value 2000 (required)
+//•	CategoryId – an integer(required)
+//•	Category – a Category object
+//•	AgentId – an integer (required)
+//•	Agent – an Agent object
+//•	RenterId – a string
+
