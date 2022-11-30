0f36383418c8268474f587d500ec27ab9a7443e2
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2022-11-30 20:39:39 +0200

    Changed Admin View

--- HouseRentingSystem/Areas/Admin/Controllers/AdminController.cs
+++ HouseRentingSystem/Areas/Admin/Controllers/AdminController.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace HouseRentingSystem.Areas.Admin.Controllers
 {
     public class AdminController : BaseController
-    {     
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
--- HouseRentingSystem/Areas/Admin/Controllers/BaseController.cs
+++ HouseRentingSystem/Areas/Admin/Controllers/BaseController.cs
@@ -5,13 +5,10 @@ using System.Data;
 namespace HouseRentingSystem.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    //[Route("/Admin/AdminController/Index/{id?}")]
+    [Route("/Admin/[controller]/[Action]/{id?}")]
     [Authorize(Roles = "Administrator")]
     public class BaseController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+       
     }
 }
